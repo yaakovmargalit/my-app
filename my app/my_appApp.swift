fd3d47ff52581dab1fca2d1e7f051b3ed5a329ab
yaakovmargalit <yaakovmargalit@gmail.com>
2024-12-29 14:29:56 +0200

    init

---
+++ my app/my_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_appApp.swift
+//  my app
+//
+//  Created by Yakov Margalit on 29/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct my_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ my appTests/my_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  my_appTests.swift
+//  my appTests
+//
+//  Created by Yakov Margalit on 29/12/2024.
+//
+
+import Testing
+@testable import my_app
+
+struct my_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
